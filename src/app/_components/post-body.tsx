53f844902bc6b1f5a1d85e3597f1da9ad59909cf
Fiore <yukihira91@proton.me>
2024-12-17 23:39:55 +0900

    feat : 자동 목차 기능 추가, h3 스타일 수정
    - 자동으로 목차를 생성해서 포스트 상단에 출력함

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,15 +1,35 @@
 "use client";
 
 import markdownStyles from "./markdown-styles.module.css";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import hljs from "highlight.js";
-import "highlight.js/styles/codepen-embed.min.css"; // 스타일 파일
+// import "highlight.js/styles/color-brewer.min.css"; // 스타일 파일
+import "highlight.js/styles/tokyo-night-dark.min.css";
 
 type Props = {
   content: string;
 };
 
+type TocItem = {
+  id: string;
+  text: string;
+  level: number;
+};
+
 export function PostBody({ content }: Props) {
+  const [toc, setToc] = useState<TocItem[]>([]);
+
+  useEffect(() => {
+    // 목차 생성
+    const headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    const tocItems: TocItem[] = Array.from(headers).map((header) => ({
+      id: header.id || header.textContent?.replace(/\s+/g, "-").toLowerCase() || "",
+      text: header.textContent || "",
+      level: parseInt(header.tagName.replace("H", ""), 10),
+    }));
+
+    setToc(tocItems);
+  }, []);
   
   useEffect(() => {
     // 코드 블록 하이라이팅 초기화
@@ -18,6 +38,23 @@ export function PostBody({ content }: Props) {
   
   return (
     <div className="max-w-2xl mx-auto">
+      {/* 목차 렌더링 */}
+      <nav className="my-4 bg-emerald-100 text-black py-4 border-l-8 border-l-emerald-600">
+        <p className="text-2xl ml-12 py-3 font-black underline">Index</p>
+        <ul className="flex flex-col gap-2 list-disc ml-4 pl-2">
+          {toc.map((item,index) => {
+            if (index === 0 || index === 1 ||index === toc.length-1) return 
+            return(
+              <li key={item.id} style={{marginLeft: `${(item.level - 1) * 16}px`}}>
+                <a href={`#${item.id}`} className="hover:underline hover:text-amber-600 hover:font-bold">
+                  {item.text}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+      {/* 본문 */}
       <div
         className={`prose lg:prose-lg ${markdownStyles["markdown"]}`}
         dangerouslySetInnerHTML={{ __html: content }}
--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,12 +1,15 @@
 import { remark } from "remark";
 import html from "remark-html";
+import toc from "remark-toc";
 import rehypePrism from "rehype-prism-plus";
+
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(html)
-    .use(rehypePrism, { showLineNumbers: true }) // Prism 문법 하이라이팅
+    .use(toc)
+    .use(rehypePrism, { showLineNumbers: true })// Prism 문법 하이라이팅
     .process(markdown);
-
+  
   return result.toString();
 }
